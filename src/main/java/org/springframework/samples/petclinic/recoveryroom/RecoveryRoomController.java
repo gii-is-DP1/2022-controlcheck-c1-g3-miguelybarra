b1b3771d018f91d01749cb66ba8dfc5e587a06c1
Miguel Ybarra <miguel.ybarra.mym@gmail.com>
2022-11-03 17:18:47 +0100

    Control check terminado

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomController.java
@@ -1,5 +1,61 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/recoveryroom")
 public class RecoveryRoomController {
+    private final RecoveryRoomService recoveryRoomService;
     
+    private static final String VIEWS_ROOM_CREATE = "recoveryroom/createOrUpdateRecoveryRoomForm";
+    private static final String VIEWS_ROOM_LISTS = "rooms/RoomList";
+    
+    @ModelAttribute("RecoveryRoomTypes")
+    public List<RecoveryRoomType> populateRoomTypes() {
+        return this.recoveryRoomService.getAllRecoveryRoomTypes();
+    }
+    
+    @Autowired
+    public RecoveryRoomController(RecoveryRoomService recoveryRoomService) {
+    	this.recoveryRoomService=recoveryRoomService;
+    }
+    
+    @GetMapping
+    public ModelAndView showRoomList() {
+        ModelAndView mav = new ModelAndView(VIEWS_ROOM_LISTS);
+        mav.addObject("rooms", recoveryRoomService.getAll());
+        return mav;
+    }
+    
+    @GetMapping("/create")
+    public ModelAndView createRoom() {
+    	ModelAndView model = new ModelAndView(VIEWS_ROOM_CREATE);
+    	model.addObject("recoveryRoom", new RecoveryRoom());
+    	return model;
+    }
+    
+    @PostMapping("/create")
+    public ModelAndView creationForm(@Valid RecoveryRoom recoveryRoom, BindingResult result) {
+    	ModelAndView model;
+    	if(result.hasErrors()) {
+    		model = new ModelAndView(VIEWS_ROOM_CREATE);
+    		model.addObject("recoveryRoom", recoveryRoom);
+    		model.addObject("types", recoveryRoomService.getAllRecoveryRoomTypes());
+    	} else {
+    		//this.recoveryRoomService.save(recoveryRoom);
+    		model=new ModelAndView("welcome");
+    	}
+    	return model;
+    }
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,21 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoomType {
-    Integer id;
+@Entity
+@Table(name="recoveryRoomType")
+public class RecoveryRoomType extends BaseEntity{
+    @NotNull
+    @Size (min = 5, max = 50)
     String name;
 }
